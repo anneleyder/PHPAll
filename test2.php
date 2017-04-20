13fa234961920df8167cfee029fd70212daee3d6
alfonso.mazza <alfonso.mazza@minfin.fed.be>
2017-04-20 15:15:35 +0200

    exercice 2 all

---
+++ test2.php
@@ -0,0 +1,14 @@
+<?php
+
+for ($i=1 ; $i < 11 ; $i++) {
+    echo "$i";
+    if ($i < 10) {
+        echo "-";
+    }
+}
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
